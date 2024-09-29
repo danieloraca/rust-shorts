79fdcb9ce455b667d8677e259c9e605459fe7d85
Dan <daniel.oraca@gmail.com>
2024-09-29 17:10:54 +0100

    simple traits for rectangle

---
+++ trait_me_softly/src/main.rs
@@ -0,0 +1,46 @@
+use std::fmt::Display;
+
+trait Area {
+    fn area(&self) -> f64;
+}
+
+trait Perimeter {
+    fn perimeter(&self) -> f32;
+}
+
+struct Rectangle {
+    width: f64,
+    heigth: f64,
+}
+
+impl Display for Rectangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Width: {}; Heigth: {}", self.width, self.heigth)
+    }
+}
+
+impl Area for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.heigth
+    }
+}
+
+impl Perimeter for Rectangle {
+    fn perimeter(&self) -> f32 {
+        2.00 * (self.width as f32 + self.heigth as f32)
+    }
+}
+
+fn main() {
+    let my_rectangle: Rectangle = Rectangle {
+        width: 1.1,
+        heigth: 2.1,
+    };
+
+    let my_rectangle_area: f64 = my_rectangle.area();
+    let my_rectangle_perimiter: f32 = my_rectangle.perimeter();
+
+    println!("My rectangle: {}", my_rectangle);
+    println!("Area of my rectangle: {}", my_rectangle_area);
+    println!("Perimiter of my retangle: {}", my_rectangle_perimiter);
+}
